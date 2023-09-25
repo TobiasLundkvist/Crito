d0209d3563f54e512db38af8951d6ca1aa1d3618
Tobias Lundkvist <tobias_lundkvist@hotmail.com>
2023-09-25 16:52:54 +0200

    started validation form

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,41 @@
+﻿using Crito.Models;
+using Crito.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Crito.Controllers
+{
+    public class ContactsController : SurfaceController
+    {
+        public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(ContactForm contactForm)
+        {
+            if(!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+           
+            return LocalRedirect(contactForm.RedirectUrl ?? "/");
+
+            
+            
+            //using var mail = new MailService();
+            // to sender
+            //mail.SendAsync(contactForm.Email, "Your request was received.", "Hi your request was received and you will be contacted soon.").ConfigureAwait(false);
+
+            // to us
+            //mail.SendAsync("tobias_lundkvist@hotmail.com", $"{contactForm.Name} requested following", contactForm.Message ).ConfigureAwait(false);
+
+        }
+    }
+}
---
+++ Models/ContactForm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crito.Models;
+
+public class ContactForm
+{
+    [Required(ErrorMessage = "You need to enter your name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "You need to enter a valid E-mail")]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "You need to enter a message")]
+    public string Message { get; set; } = null!;
+
+    public string? RedirectUrl { get; set; } = "/";
+}
